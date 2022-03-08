53bb886fcfa2a41b157378191be10764f058e7b8
ErikFerreira477 <erikcalcada@hotmail.com>
2022-03-07 22:20:56 -0300

    completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,38 @@
 import "./Style.css";
 import "antd/dist/antd.min.css";
+import axios from "axios";
 import { List, Card } from "antd";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const data = [
-    {
-      title: "Title 1",
-    },
-    {
-      title: "Title 2",
-    },
-    {
-      title: "Title 3",
-    },
-    {
-      title: "Title 4",
-    },
-    {
-      title: "Title 5",
-    },
-    {
-      title: "Title 6",
-    },
-    {
-      title: "Title 7",
-    },
-    {
-      title: "Title 8",
-    },
-  ];
+  const [poke, setPoke] = useState<Array<String>>([]);
+
+  useEffect(() => {
+    getPokes();
+  }, []);
+
+  const getPokes = () => {
+    axios({
+      method: "get",
+      url: "https://pokeapi.co/api/v2/pokemon?limit=40",
+    }).then(function (response) {
+      response.data.results.forEach(function (poke: any) {
+        setPoke((oldPokes: any) => [...oldPokes, poke.name]);
+      });
+    });
+  };
+
+  console.log(poke);
 
   return (
     <div className="center">
       <List
         grid={{ gutter: 16, column: 4 }}
-        dataSource={data}
+        dataSource={poke}
         pagination={{ pageSize: 4 }}
         renderItem={(item) => (
           <List.Item>
-            <Card title={item.title}>Card content</Card>
+            <Card>{item}</Card>
           </List.Item>
         )}
       />
